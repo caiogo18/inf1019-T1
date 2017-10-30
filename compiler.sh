7c1b5717f01c22cc80f13c931a6ff2070316cbe7
Tijuk <tijuk00@hotmail.com>
2017-10-29 23:12:32 -0200

    Update compiler.sh

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,3 @@
-gcc -Wall -o esc esc_mlf.c Fila.c aux.c
-gcc -Wall -o int interpretador.c aux.c
+gcc -Wall -o esc esc_mlf.c Fila.c auxiliar.c
+gcc -Wall -o int interpretador.c auxiliar.c
 gcc -Wall -o programa programa.c
